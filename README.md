c182fad3c95eb1db484764695380170e7e9928f7
Wolfy-J <wolfy.jd@gmail.com>
2017-02-03 20:23:43 +0300

    Split from components.

---
+++ README.md
@@ -0,0 +1,9 @@
+Spiral ORM
+========
+[![Latest Stable Version](https://poser.pugx.org/spiral/orm/v/stable)](https://packagist.org/packages/spiral/orm) 
+[![License](https://poser.pugx.org/spiral/orm/license)](https://packagist.org/packages/spiral/orm)
+[![Build Status](https://travis-ci.org/spiral/orm.svg?branch=master)](https://travis-ci.org/spiral/orm)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/spiral/orm/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/spiral/orm/?branch=master)
+[![Coverage Status](https://coveralls.io/repos/github/spiral/orm/badge.svg?branch=master)](https://coveralls.io/github/spiral/orm?branch=master)
+
+<b>[Documentation](http://spiral-framework.com/guide)</b> | [CHANGELOG](/CHANGELOG.md)
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,42 +15,4 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 //File component fixtures
 define('FIXTURE_DIRECTORY', __DIR__ . '/Files/fixtures/');
-
-if (!function_exists('env')) {
-    function env(string $key, $default = null)
-    {
-        $value = getenv($key);
-        if (is_null($value)) {
-            return $default;
-        }
-
-        switch (strtolower($value)) {
-            case 'true':
-            case '(true)':
-                return true;
-
-            case 'false':
-            case '(false)':
-                return false;
-
-            case 'null':
-            case '(null)':
-                return null;
-
-            case 'empty':
-            case '(empty)':
-                return '';
-        }
-
-        return $value;
-    }
-}
-
-//Always keep it outside of repo.
-if (file_exists(dirname(__DIR__) . '/.env')) {
-    $loader = new \Dotenv\Loader(dirname(__DIR__) . '/.env');
-    $loader->load();
-}
-
-//Set to true to enter the Matrix
-define('ENABLE_PROFILING', env('PROFILING', false));
+define('ENABLE_PROFILING', getenv('PROFILING') ?? false);
